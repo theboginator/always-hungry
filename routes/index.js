e99ea4a3164a0c0bf83b5bf6cc99c3dccc103657
Jacob Bogner <boginator55@gmail.com>
2019-12-17 02:56:47 -0500

    Working preview is ready
    Still need to get on heroku

--- routes/index.js
+++ routes/index.js
@@ -18,19 +18,19 @@ router.get('/', function(req, res, next) {
 
 /* GET users listing. */
 router.get('/login', function(req, res, next) {
-  res.render('login');
+  res.render('login',  {error: req.flash('error')});
 
 });
 
 router.post('/login',
   // depends on the fiels "isAdmin", redirect to the different path: admin or notAdmin
-  passport.authenticate('local', { failureRedirect: '/', failureFlash:true }),
+  passport.authenticate('local', { failureRedirect: '/login', failureFlash:true }),
   function(req, res, next) {
     if (req.user.isadmin == 'admin'){
       res.redirect('/admin');
     }
     else {
-      res.redirect('/profile');
+      res.redirect('/');
     }
 });
 
@@ -99,45 +99,10 @@ function averageReview(result, type){
   console.log(result.rowCount);
   var average = 0;
   for(var ctr=0; ctr<result.rowCount; ctr++){
-    average = average + result.rows[ctr][type];
+    average = average + result.rows[ctr];
   }
   return (average/result.rows.height);
 }
-
-router.get('/notAdmin',loggedIn,function(req, res, next){
-  if(req.user.isadmin !="admin"){
-  client.query('SELECT * FROM ticket WHERE username=$1 order by status asc',[req.user.username], function(err,result){
-    if (err) {
-      console.log("exam.js: sql error ");
-      next(err); // throw error to error.hbs.
-    }
-    else if (result.rows.length > 0) {
-      console.log("Here I am");
-      res.render('notAdmin', {rows: result.rows, user: req.user} );
-    }
-    else{
-      console.log("This student does not have any assignment");
-      res.render('notAdmin', {rows: result.rows, user: req.user} );
-
-    }
-  });
-}
-else{
-res.redirect('/admin');
-}
-});
-
-
-router.get('/admin',loggedIn,function(req, res){
-  if(req.user.isadmin=='admin'){
-      res.render('admin', { user: req.user }); 
-  }
-  else{
-    res.redirect('/notAdmin');
-  }
-});
-
-
 
 router.get('/signup',function(req, res) {
   res.render('signup', { user: req.user }); // signup.hbs
@@ -147,6 +112,7 @@ router.get('/signup',function(req, res) {
 
 router.post('/signup', function(req, res, next) {
   console.log(req.body);
+  console.log("signing a new user up");
   client.query('SELECT * FROM users WHERE username = $1', [req.body.username], function(err, result) {
     if (err) {
       console.log("unable to query SELECT");
@@ -156,6 +122,7 @@ router.post('/signup', function(req, res, next) {
       res.render('signup', { user: req.user,exist:"true" });
     }
     else{
+      console.log("Attempting insert");
       client.query('INSERT INTO users (username, password, level) VALUES($1, $2, $3)', [req.body.username,encryptPWD(req.body.password), 'student'], function(err, result) {
         if (err) {
           console.log("unable to query INSERT");
@@ -168,28 +135,34 @@ router.post('/signup', function(req, res, next) {
   });
 });
 
-router.get('/adminDashboard', function(req, res, next) {
-  console.log(req.user.isadmin);
-  if(req.user.isadmin=="admin"){
-    client.query('SELECT * FROM ticket order by status asc', function(err, result){
-        if(err){ next(err);}
-        res.render('adminDashboard', result)
-    });
-  }
-  else{
-    res.redirect('/notAdmin');
-  }
+router.get('/review',function(req, res, next) {
+  res.render('review', {user: req.user, error: req.flash('error')});
 });
 
-router.post('/adminDashboard',function(req, res) {
-  client.query('Update ticket set status = $1 where id = $2', [req.body.status, req.body.key], function(err, result) {
+//Add ticket to ticket database
+router.post('/review',function(req, res, next) {
+  client.query('SELECT * FROM users WHERE username = $1', [req.user.username], function(err, result) {
     if (err) {
-      console.log("unable to query Update");
       next(err);
     }
+    if (result.rows.length > 0) {
+      console.log("Attempting to insert to database");
+        client.query('INSERT INTO reviews (username, published, hall, service, speed, food, busy, comment) VALUES($1, $2, $3, $4, $5, $6, $7, $8)', [req.user.username, '2019-12-17', req.body.hall, req.body.service, req.body.speed, req.body.busy, req.body.busy, req.body.comment], function(err, result) {
+          console.log("Passed insert statement");
+          if (err) {
+            next(err);
+            console.log(err);
+          }
+          console.log("Insertion successful, re-loading page");
+          res.render('review', {user: req.user , success: "true" });
+        });
+    }
+    else{
+      res.render('signup', {user: req.user ,error: "Username doesnt exist"});
+    }
   });
-  res.redirect('/adminDashboard'); 
 });
+
 /*
 router.get('/crimtest', function(req, response, next) {
   client.query("SELECT username, published, comment FROM reviews WHERE hall = 'crimson' ORDER BY published DESC", function(err, result) {
@@ -204,9 +177,7 @@ router.get('/crimtest', function(req, response, next) {
 
 router.get('/crimson',function(req, response, res) {
 
-  
-  
-  client.query("SELECT username, published, comment FROM reviews WHERE hall = 'crimson' ORDER BY published DESC", function(err, result) {
+  client.query("SELECT username, published, comment, service, speed, food, busy FROM reviews WHERE hall = 'crimson' ORDER BY published DESC", function(err, result) {
     if (err) {
       console.log("unable to query SELECT for crimson dining");
       next(err);
@@ -215,55 +186,57 @@ router.get('/crimson',function(req, response, res) {
     console.log("Queried for crimson data, got: ");
     console.log(result.rows);
     console.log(result.rowCount);
-
-    /*
-    service = averageReview(result, 'service');
-    speed = averageReview(result, 'speed');
-    food = averageReview(result, 'food');
-    busy = averageReview(result, 'busy');
-    console.log("service average:");
-    console.log(service);
-    */
     
     response.render('crimson', result); // Load the crimson review page with the reviews table
-
-
-  
-    if(result.rows.length > 0){
-      //load in review data for stuff
-    }
-    else{
-      //There are no reviews; set data values to 0
-    }
   });
-  
-/*
-  console.log("Queried for crimson data, got: ");
-  console.log(result);
-  service = averageReview(result, 0);
-  speed = averageReview(result, 1);
-  food = averageReview(result, 2);
-  busy = averageReview(result, 3);
-  console.log(service);
-  res.render('crimson', { user: req.user }); // Load the crimson review page with the reviews table
-
-*/
   
 });
 
 router.get('/eastCampusCrap',function(req, res) {
-  res.render('eastCampusCrap', { user: req.user }); // eastCampusCrap.hbs
+  client.query("SELECT username, published, comment, service, speed, food, busy FROM reviews WHERE hall = 'eastCampusCrap' ORDER BY published DESC", function(err, result) {
+    if (err) {
+      console.log("unable to query SELECT for ECC dining");
+      next(err);
+    }
+    
+    console.log("Queried for ECC data, got: ");
+    console.log(result.rows);
+    console.log(result.rowCount);
+    
+    res.render('eastCampusCrap', result); // Load the ECC review page with the reviews table
+  });
   
 });
 
 router.get('/bearsDen',function(req, res) {
-  res.render('bearsDen', { user: req.user }); // bearsDen.hbs
+  client.query("SELECT username, published, comment, service, speed, food, busy FROM reviews WHERE hall = 'bearsDen' ORDER BY published DESC", function(err, result) {
+    if (err) {
+      console.log("unable to query SELECT for Bear's Den");
+      next(err);
+    }
+    
+    console.log("Queried for Bear's Den data, got: ");
+    console.log(result.rows);
+    console.log(result.rowCount);
+    
+    res.render('bearsDen', result); // Load the bears den review page with the reviews table
+  });
   
 });
 
 router.get('/tilly',function(req, res) {
-  res.render('tilly', { user: req.user }); // tilly.hbs
-  
+  client.query("SELECT username, published, comment, service, speed, food, busy FROM reviews WHERE hall = 'tilly' ORDER BY published DESC", function(err, result) {
+    if (err) {
+      console.log("unable to query SELECT for tilly dining");
+      next(err);
+    }
+    
+    console.log("Queried for tilly data, got: ");
+    console.log(result.rows);
+    console.log(result.rowCount);
+    
+    res.render('tilly', result); // Load the tilly review page with the reviews table
+  });
 });
 
 module.exports = router;
